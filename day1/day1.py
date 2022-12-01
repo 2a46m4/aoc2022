06b93a0489f49384661bd3a67bc3dc21772ae386
dabanya02 <gcciscool@protonmail.com>
2022-12-01 00:08:24 -0800

    now it's a true one-liner

--- day1/day1.py
+++ day1/day1.py
@@ -1,2 +1,1 @@
-from functools import reduce
-print(reduce(max, map(lambda z : reduce(lambda a, b : a + b, map(int, z)), map(lambda s:s.split("\n"),open("input.txt", "r").read().strip('\n').split("\n\n")))))
+print(max(map(lambda z : sum(map(int, z)), map(lambda s:s.split("\n"),open("input.txt", "r").read().strip('\n').split("\n\n")))))
