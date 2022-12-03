188d0f10cddda76bb4d359daddc7fa0da46b24f4
dabanya02 <gcciscool@protonmail.com>
2022-12-03 02:03:08 -0800

    sdlfkjlkdsjfl

--- day3/day3.py
+++ day3/day3.py
@@ -3,24 +3,19 @@ def day3pt1():
     for line in open("input.txt").read().split("\n"):
         first = line[:int(len(line)/2)]
         second = line[int(len(line)/2):]
-        dict = set()
-        for item in first:
-            dict.add(item)
-        for item in second:
-            if item in dict:
-                if item.isupper():
-                    ans+= ord(item) - 38
-                else:
-                    ans+= ord(item) - 96
-                break
+        val = (set(first) & set(second)).pop()
+        if val.isupper():
+            ans+= ord(val) - 38
+        else:
+            ans+= ord(val) - 96
+            break
     print(ans)
 
-
-
+    
 def day3pt2(lines):
     if(len(lines) == 0):
         return 0
-    ans = set(lines[0]).intersection(set(lines[1]).intersection(set(lines[2]))).pop()
+    ans = (set(lines[0]) & set(lines[1]) & set(lines[2])).pop()
     if ans.isupper():
         return ord(ans) - 38 + day3pt2(lines[3:])
     else:
