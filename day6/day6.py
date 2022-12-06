aa7bbe16b63383f0a89c8051fecf2567d646a736
dabanya02 <gcciscool@protonmail.com>
2022-12-06 03:46:10 -0800

    day6

--- day6/day6.py
+++ day6/day6.py
@@ -1,18 +1,20 @@
 def part1():
-    text = open("input.txt").read().replace('\n','')
-    for i in range(len(text)):
-        dict = set([text[i], text[i+1], text[i+2], text[i+3]])
-        print (dict)
+    text = open("input2.txt").read().replace('\n','')
+    i = 0
+    while(i < len(text)):
+        dict = set(text[i:i+4])
         if len(dict) == 4:
             return i + 4
+        i+=1
+        
 def part2():
-    text = open("input.txt").read().replace('\n','')
-    for i in range(len(text)):
+    text = open("input2.txt").read().replace('\n','')
+    i = 0
+    while(i < len(text)):
         dict = set(text[i:i+14])
-        print(dict)
         if len(dict) == 14:
             return i + 14
-
+        i+=1
 
 print(part1())
 print(part2())
